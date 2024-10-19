bbccef94f4f07e85eac076c75e0a1cfa05040ee5
Yakuberr <YakubKr@proton.me>
2024-10-19 17:17:37 +0200

    Przenoszę wrapper dotyczący API sejmu do odpowiedniego modułu

---
+++ sejmAPI/sejm/__init__.py
@@ -0,0 +1,9 @@
+from . import clubs
+from . import committees
+from . import interpellations
+from . import mp
+from . import prints
+from . import terms
+# from . import utils
+from . import videos
+from . import votings
